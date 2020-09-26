4ce01d31008b6a6eddd4c54928e9d00194e34edf
hmartiins <hmartins224@gmail.com>
2020-09-26 01:25:29 -0300

    creating method of authorizing the token

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+class Auth {
+  async auth(request, response, next){
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      return response.status(401).json({
+        message: 'Token is require',
+      })
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+      await jwt.verify(token, config.APP_SECRET);
+
+      next();
+    } catch (err) {
+      return response.status(401).json({
+        message: 'Token invalid',
+      })
+    }
+  }
+}
+
+module.exports = Auth;
--- server/src/routes.js
+++ server/src/routes.js
@@ -3,14 +3,16 @@ const { Router } = require('express');
 const routes = Router();
 
 const UserController = require('./controllers/UserController');
+const Auth = require('./middlewares/auth');
 
+const auth = new Auth(); 
 const userController = new UserController();
 
 routes.post('/login', userController.login);
 
 routes.post('/user', userController.create);
 routes.get('/user/:cd_user', userController.show);
-routes.delete('/user/:cd_user', userController.delete);
-routes.put('/user/:cd_user', userController.update);
+routes.delete('/user/:cd_user', auth.auth, userController.delete);
+routes.put('/user/:cd_user', auth.auth, userController.update);
 
 module.exports = routes;
