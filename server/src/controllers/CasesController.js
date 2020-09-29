a6203988ceb6fe39404fc805eef71c24dca93fa6
hmartiins <hmartins224@gmail.com>
2020-09-28 22:20:22 -0300

    applying that function checkField to the CasesController

--- server/src/controllers/CasesController.js
+++ server/src/controllers/CasesController.js
@@ -3,11 +3,20 @@ const jwt = require('jsonwebtoken');
 const knex = require('../models/connection');
 const config = require('../config/config');
 
+const checkField = require('../utils/index');
+
 class CasesController {
   async create(request, response){
     const { title, description } = request.body;
 
     const trx = await knex.transaction();
+
+    const userEmailExisting = await checkField('tb_cases', 'title', title);
+    const userCpfExisting = await checkField('tb_cases', 'description', description);
+
+    if (userEmailExisting === true || userCpfExisting === true) {
+      return response.status(409).send({ error: 'Conflict between fields !' });
+    }
 
     try {
       await trx('tb_cases').insert({title, description});
@@ -24,6 +33,12 @@ class CasesController {
   }
   async delete(request, response) {
     const { cd_cases } = request.params;
+
+    const userVerification = await checkField('tb_cases', 'cd_case', cd_case);
+
+    if (userVerification === false) {
+      return response.status(404).send({ error: 'Case not found' });
+    }
 
     try {
       await knex('tb_cases')
